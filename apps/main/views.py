958444b16a58b7965afa3c4ccf6b913ddd2d08d3
sadra <sadraabadkar1383@gmail.com>
2025-10-01 10:16:02 +0330

    first commit

---
+++ apps/main/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import web.settings as sett
+from django.utils import timezone
+# Create your views here.
+
+
+
+
+def media_admin(request):
+    context = {
+        'media_url':sett.MEDIA_URL
+    }
+
+
+    return context
+
+
+def main(request):
+
+    return render(request,'main_app/main.html')
